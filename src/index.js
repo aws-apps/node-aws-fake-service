362a846124c1e3621390e7407a077d9e9e600238
Pradeep Kumar <pradeepk.pdp@gmail.com>
2021-08-01 09:39:56 +0530

    fix: port number for EBS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to node-aws-fake-service!!');
 });
 
-app.listen(3000, () => {
-    console.log('node-aws-fake-service running at port: 3000');
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`node-aws-fake-service running at port: ${port}`);
 });
